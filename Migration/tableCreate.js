99197167b72c6c60dcfb6086dbe5fa55e8d69fed
Benjamin <bthevenot@yahoo.com>
2026-02-24 17:52:52 +0100

    Tables correction
    fusion character and character stats
    fusion mob and mob stats
    fusion items and items stats

--- Migration/tableCreate.js
+++ Migration/tableCreate.js
@@ -8,7 +8,7 @@ con.connect(function (err) {
   console.log("Connected!");
 
   con.query(
-    "CREATE TABLE IF NOT EXISTS characters (id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, name varchar(25), gender enum('Man', 'Woman', 'Other'), image varchar(255), category enum('Main-Character', 'Ally','Enemy'));",
+    "CREATE TABLE IF NOT EXISTS characters (id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, name varchar(25), gender enum('Man', 'Woman', 'Other'), image varchar(255), category enum('Main-Character', 'Ally','Enemy'), level int(99), ATK int(99), DEF int(99), Health int(99), MANA int(99), speed int(11), atb_jauge int(99), magic int(99), spirit int(99), dodge int(99), accuracy int(99));",
     function (err) {
       if (err) throw err;
       console.log("Table created");
@@ -16,23 +16,7 @@ con.connect(function (err) {
   );
 
   con.query(
-    "CREATE TABLE IF NOT EXISTS characters_stats (id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, character_id int(11), level int(99), ATK int(99), DEF int(99), Health int(99), MANA int(99),  speed int(11), atb_jauge int(99), magic int(99), spirit int(99), dodge int(99), accuracy int(99));",
-    function (err) {
-      if (err) throw err;
-      console.log("Table created");
-    },
-  );
-
-  con.query(
-    "CREATE TABLE IF NOT EXISTS items (id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, name varchar(50), category enum('key_item', 'weapon', 'armor', 'consumable', 'spell'), description text(255), image varchar(255));",
-    function (err) {
-      if (err) throw err;
-      console.log("Table created");
-    },
-  );
-
-  con.query(
-    "CREATE TABLE IF NOT EXISTS items_stats (id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, item_id int(11), ATK_bonus int(11), DEF_bonus int(11), MANA_bonus int(11), Health_bonus int(11), Speed_bonus int(11));",
+    "CREATE TABLE IF NOT EXISTS items (id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, name varchar(50), category enum('key_item', 'weapon', 'armor', 'consumable', 'spell'), description text(255), image varchar(255), ATK_bonus int(11), DEF_bonus int(11), MANA_bonus int(11), Health_bonus int(11), Speed_bonus int(11));",
     function (err) {
       if (err) throw err;
       console.log("Table created");
@@ -48,15 +32,7 @@ con.connect(function (err) {
   );
 
   con.query(
-    "CREATE TABLE IF NOT EXISTS mobs (id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, name varchar(25), gender enum('Man', 'Woman', 'Other'), image varchar(255), category enum('Main-Character', 'Ally', 'Enemy'));",
-    function (err) {
-      if (err) throw err;
-      console.log("Table created");
-    },
-  );
-
-  con.query(
-    "CREATE TABLE IF NOT EXISTS mobs_stats (id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, mobs_id int(11), level int(99), ATK int(99), DEF int(99), Health int(99), MANA int(99), speed int(11), atb_jauge int(11), magic int(99), spirit int(99), dodge int(99), accuracy int(99), DMG int(99));",
+    "CREATE TABLE IF NOT EXISTS mobs (id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, name varchar(25), gender enum('Man', 'Woman', 'Other'), image varchar(255), category enum('Main-Character', 'Ally', 'Enemy'), level int(99), ATK int(99), DEF int(99), Health int(99), MANA int(99), speed int(11), atb_jauge int(11), magic int(99), spirit int(99), dodge int(99), accuracy int(99), DMG int(99));",
     function (err) {
       if (err) throw err;
       console.log("Table created");
@@ -89,22 +65,6 @@ con.connect(function (err) {
 
   con.query(
     "CREATE TABLE IF NOT EXISTS worldmap (id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, name varchar(25), image varchar(255), cycle enum('day', 'night'), type_zone enum('City', 'Land', 'Cave', 'Desert'));",
-    function (err) {
-      if (err) throw err;
-      console.log("Table created");
-    },
-  );
-
-  con.query(
-    "CREATE TABLE IF NOT EXISTS worldmap (id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, name varchar(25), image varchar(255), cycle enum('day', 'night'), type_zone enum('City', 'Land', 'Cave', 'Desert'));",
-    function (err) {
-      if (err) throw err;
-      console.log("Table created");
-    },
-  );
-
-  con.query(
-    "CREATE TABLE IF NOT EXISTS worldmap_items (id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, worldmap_id int(11), item_id int(11), quantity int(11));",
     function (err) {
       if (err) throw err;
       console.log("Table created");
