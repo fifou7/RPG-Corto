7bdcf7c22c1ec6e8fb45d7fabdbd3cd2fa28fee8
Famah <syllasmn1@gmail.com>
2026-02-26 17:40:00 +0100

    Ajoutd de l'image de fond de combat

--- Public/Mouvements-perso/scrypte.js
+++ Public/Mouvements-perso/scrypte.js
@@ -2,8 +2,8 @@ console.log('yo');
 
 const config = {  
 type: Phaser.AUTO, 
-height : 600, 
-width: 800, 
+height : window.innerHeight, 
+width: window.innerWidth, 
 parent: 'game-container',
 scene : {
     preload : preload,
@@ -12,7 +12,7 @@ scene : {
 },
 scale: {
 
-        mode: Phaser.Scale.FIT,
+        mode: Phaser.Scale.RESIZE,
         autoCenter: Phaser.Scale.CENTER_BOTH,
         width: '100%',
         height: '100%'
@@ -25,13 +25,21 @@ function preload() {
     this.load.image("Tidus","../Images/Tidus.png");
     this.load.image("Lunafreya","../Images/Lunafreya.png");
     this.load.image("Sora","../Images/Sora.png");
+    this.load.image("background-fight","../Images/image-de-fond-fight.png");
 
 }
 
 function create() {
-    this.add.sprite(280,600, "Tidus");
-    this.add.sprite(200,350, "Lunafreya");
-    this.add.sprite(250,850, "Sora");
+    var backgroundImage = this.add.sprite(0,0, "background-fight");
+    backgroundImage.setDisplaySize(config.width, config.height);
+    backgroundImage.setPosition(config.width/2, config.height/2);
+    let Tidus = this.add.sprite(495,595, "Tidus");
+    Tidus.setScale(1.25);
+    let Lunafreya = this.add.sprite(400,350, "Lunafreya");
+    Lunafreya.setScale(1.25);
+    let Sora = this.add.sprite(440,860, "Sora");   
+    Sora.setScale(1.25);
+
 
 }
 
