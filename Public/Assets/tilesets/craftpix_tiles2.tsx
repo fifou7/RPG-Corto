5b0eb326b64abdd07302bfb15fcfacca70f0c8d5
Benjamin <bthevenot@yahoo.com>
2026-03-02 00:53:33 +0100

    Création map tiled
    - ajout tiles sets craftpix
    -  ajout de la map en JSON
    
    reste à lier le tout à phaser

---
+++ Public/Assets/tilesets/craftpix_tiles2.tsx
@@ -0,0 +1,199 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="craftpix_tiles2" tilewidth="256" tileheight="256" tilecount="65" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_01.png" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_02.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_03.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_04.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_05.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_06.png" width="32" height="32"/>
+ </tile>
+ <tile id="6">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_07.png" width="32" height="32"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_08.png" width="32" height="32"/>
+ </tile>
+ <tile id="8">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_09.png" width="32" height="32"/>
+ </tile>
+ <tile id="9">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_10.png" width="32" height="32"/>
+ </tile>
+ <tile id="10">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_11.png" width="32" height="32"/>
+ </tile>
+ <tile id="11">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_12.png" width="32" height="32"/>
+ </tile>
+ <tile id="12">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_13.png" width="32" height="32"/>
+ </tile>
+ <tile id="13">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_14.png" width="32" height="32"/>
+ </tile>
+ <tile id="14">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_15.png" width="32" height="32"/>
+ </tile>
+ <tile id="15">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_16.png" width="32" height="32"/>
+ </tile>
+ <tile id="16">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_17.png" width="32" height="32"/>
+ </tile>
+ <tile id="17">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_18.png" width="32" height="32"/>
+ </tile>
+ <tile id="18">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_19.png" width="32" height="32"/>
+ </tile>
+ <tile id="19">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_20.png" width="32" height="32"/>
+ </tile>
+ <tile id="20">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_21.png" width="32" height="32"/>
+ </tile>
+ <tile id="21">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_22.png" width="32" height="32"/>
+ </tile>
+ <tile id="22">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_23.png" width="32" height="32"/>
+ </tile>
+ <tile id="23">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_24.png" width="32" height="32"/>
+ </tile>
+ <tile id="24">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_25.png" width="32" height="32"/>
+ </tile>
+ <tile id="25">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_26.png" width="32" height="32"/>
+ </tile>
+ <tile id="26">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_27.png" width="32" height="32"/>
+ </tile>
+ <tile id="27">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_28.png" width="32" height="32"/>
+ </tile>
+ <tile id="28">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_29.png" width="32" height="32"/>
+ </tile>
+ <tile id="29">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_30.png" width="32" height="32"/>
+ </tile>
+ <tile id="30">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_31.png" width="32" height="32"/>
+ </tile>
+ <tile id="31">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_32.png" width="32" height="32"/>
+ </tile>
+ <tile id="32">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_33.png" width="32" height="32"/>
+ </tile>
+ <tile id="33">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_34.png" width="32" height="32"/>
+ </tile>
+ <tile id="34">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_35.png" width="32" height="32"/>
+ </tile>
+ <tile id="35">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_36.png" width="32" height="32"/>
+ </tile>
+ <tile id="36">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_37.png" width="32" height="32"/>
+ </tile>
+ <tile id="37">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_38.png" width="32" height="32"/>
+ </tile>
+ <tile id="38">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_39.png" width="32" height="32"/>
+ </tile>
+ <tile id="39">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_40.png" width="32" height="32"/>
+ </tile>
+ <tile id="40">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_41.png" width="32" height="32"/>
+ </tile>
+ <tile id="41">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_42.png" width="32" height="32"/>
+ </tile>
+ <tile id="42">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_43.png" width="32" height="32"/>
+ </tile>
+ <tile id="43">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_44.png" width="32" height="32"/>
+ </tile>
+ <tile id="44">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_45.png" width="32" height="32"/>
+ </tile>
+ <tile id="45">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_46.png" width="32" height="32"/>
+ </tile>
+ <tile id="46">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_47.png" width="32" height="32"/>
+ </tile>
+ <tile id="47">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_48.png" width="32" height="32"/>
+ </tile>
+ <tile id="48">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_49.png" width="32" height="32"/>
+ </tile>
+ <tile id="49">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_50.png" width="32" height="32"/>
+ </tile>
+ <tile id="50">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_51.png" width="32" height="32"/>
+ </tile>
+ <tile id="51">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_52.png" width="32" height="32"/>
+ </tile>
+ <tile id="52">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_53.png" width="32" height="32"/>
+ </tile>
+ <tile id="53">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_54.png" width="32" height="32"/>
+ </tile>
+ <tile id="54">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_55.png" width="32" height="32"/>
+ </tile>
+ <tile id="55">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_56.png" width="32" height="32"/>
+ </tile>
+ <tile id="56">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_57.png" width="32" height="32"/>
+ </tile>
+ <tile id="57">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_58.png" width="32" height="32"/>
+ </tile>
+ <tile id="58">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_59.png" width="32" height="32"/>
+ </tile>
+ <tile id="59">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_60.png" width="32" height="32"/>
+ </tile>
+ <tile id="60">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_61.png" width="32" height="32"/>
+ </tile>
+ <tile id="61">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_62.png" width="32" height="32"/>
+ </tile>
+ <tile id="62">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_63.png" width="32" height="32"/>
+ </tile>
+ <tile id="63">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tile2_64.png" width="32" height="32"/>
+ </tile>
+ <tile id="64">
+  <image source="../../../../../../Downloads/craftpix-net-504452-free-village-pixel-tileset-for-top-down-defense/1.1 Tiles/Tileset2.png" width="256" height="256"/>
+ </tile>
+</tileset>
